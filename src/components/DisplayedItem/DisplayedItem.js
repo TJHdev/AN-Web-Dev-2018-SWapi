64ea735ab42b29e868c02cfdf806f0e8aea2f007
Thomas Hanna <thomasjhanna@gmail.com>
2018-10-03 01:42:25 +0100

    Fixed bug associated with API change

--- src/components/DisplayedItem/DisplayedItem.js
+++ src/components/DisplayedItem/DisplayedItem.js
@@ -1,44 +1,38 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './DisplayedItem.css'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./DisplayedItem.css";
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     data: state.requestData.data,
     itemSelected: state.requestData.itemSelected
-  }
-}
+  };
+};
 
 class DisplayedItem extends Component {
-
   render() {
     let itemToDisplay;
-    if(this.props.data.results) {
+    if (this.props.data.results) {
       itemToDisplay = this.props.data.results[this.props.itemSelected];
       console.log(itemToDisplay);
-      console.log()
 
       return (
         <table className="displayeditem__container">
           <tbody>
-            {Object.entries(itemToDisplay).map((pair) => {
-              return <tr className="displayeditem__row"><td className="displayeditem__key">{pair[0]}</td><td className="displayeditem__value">{pair[1]}</td></tr>
+            {Object.entries(itemToDisplay).map(pair => {
+              return (
+                <tr className="displayeditem__row">
+                  <td className="displayeditem__key">{pair[0]}</td>
+                  <td className="displayeditem__value">{pair[1]}</td>
+                </tr>
+              );
             })}
           </tbody>
         </table>
       );
     }
 
-    return (
-      <div>
-        Select a category
-      </div>
-    )
-    
-    
-    
-
-    
+    return <div>Select a category</div>;
   }
 }
 
